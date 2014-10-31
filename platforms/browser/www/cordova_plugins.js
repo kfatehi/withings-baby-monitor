faedf624021c21b7457d30548373bd230c810245
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-31 23:12:16 +0100

    init commit

---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,9 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.inappbrowser": "0.5.3"
+}
+// BOTTOM OF METADATA
+});
